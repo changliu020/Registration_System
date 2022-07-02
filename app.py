472a547c0920f07ad0423f30116bc54d32b0e033
changliu020 <changliu020@gmail.com>
2022-07-02 16:45:11 -0700

    fix import error

--- app.py
+++ app.py
@@ -1,3 +1,7 @@
+import pymysql
+pymysql.install_as_MySQLdb()
+
+
 from email.mime import application
 from flask import Flask, render_template, request, redirect, url_for, session
 from flask_mysqldb import MySQL
